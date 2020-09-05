a999837dd17732463485ce997af17938548cf579
jayson911 <jayson406@gmail.com>
2020-09-05 21:49:51 +0300

    ts

--- vue/lesson1/lesson1/src/App.vue
+++ vue/lesson1/lesson1/src/App.vue
@@ -1,28 +1,62 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App1"/>
+  <div>
+    <button v-on:click="increase">
+      Click me
+    </button>
+    <p>{{counter}}</p>
   </div>
+
+<!--  v-on:mousemove-->
+<!--  <div>-->
+<!--    <button v-on:click="increase">Click me</button>-->
+<!--    <p v-on:mousemove="updateCoordinates"> Coordinates: {{ x }} / {{ y }}</p>-->
+<!--  </div>-->
+
+<!--  Modifieng events >-->
+
+<!--  <div>-->
+<!--    <p v-on:mousemove="updateCoordinates">-->
+<!--      Coordinates: {{ x }} / {{ y }}-->
+<!--      - <span v-on:mousemove.stop>DEAD SPOT</span>-->
+<!--    </p>-->
+<!--    <input type="text" v-on:keyup.enter.space="alert">-->
+<!--  </div>-->
+
+<!--  Only for read-->
+<!--  Writting js code in templates -->
+<!--  Show increase  <button v-on:click="increase++">Click me</button>-->
+<!--  String interpolation {{ counter * 2 }}-->
+<!--  Ternar operation {{ counter * 2 > 10 ? 'Greater than 10': 'Smaller than 10'}}-->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'Events',
+
+  data() {
+    return {
+      counter: 0,
+    }
+  },
+
+  methods: {
+    increase() {
+      this.counter++;
+    },
+
+    updateCoordinates(event) {
+      console.log(event);
+      this.x = event.clientX;
+      this.y = event.clientY;
+    },
+
+    alert() {
+      alert('Alert!!!!');
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
